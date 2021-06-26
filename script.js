877aaa5b743825ca95c40b5c8f7445915b0a6c5e
Diamond Rosales <roserosales288@gmail.com>
2021-06-25 23:18:56 -0400

    final upload of planning hw

---
+++ script.js
@@ -0,0 +1,17 @@
+const calendar = document.querySelector("app-calendar");
+
+//console.log("calendar") checking to see if the calendar will show up 
+
+//need to create a boolean to locate the weekends in the month
+//how to create for all saturdays& sundays in the month?
+
+var weekend = day ! = {
+
+}
+const date= Date.new Date(Date.UTC(2021, 0, day)); 
+const dayName = new Intl.DateTimeFormat("en-US", options). format(date);
+console.log(dayName)
+
+for (var day = 1; day < 30; day ++) {
+   // console.log("day") [1] + " . "
+}
